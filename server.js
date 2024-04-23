8f1c4da9685a0ec99bcb807f51bc79d98fe29b43
Harryje <harryjptucker@gmail.com>
2024-04-23 19:29:57 +0100

    Added games page

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const words = ['games', 'mc', 'wiki', 'dl', 'crafty', 'vault', 'rust' ];
+const words = ['games', 'wiki', 'dl', 'crafty', 'vault',  ];
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index', { domains: words });
 });
+
+app.get('/games', (req, res) => {
+    res.render('games');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
